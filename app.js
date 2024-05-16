76766a1f5c80ec8e40974de08813dc68684c22ef
maurilioreis7 <devnet@sistemafieto.rede>
2024-05-16 11:06:03 -0300

    meu primeiro commit

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express();
-
-app.use((req, res, next)=>{
-    res.send("minha api")
+const usuario=[
+    {nome:'carlos', idade:'20',peso:'100'},
+    {nome:'pedro', idade:'30'},
+    {nome: 'joao', idade: '18', peso: '75', cep: '77807060'},
+]
+app.use("/",(req, res, next)=>{
+    res.send(usuario)
+})
+app.use("/nome",(req, res, next)=>{
+    res.send(usuario[1].nome)
+})
+app.use("/soma",(req, res, next)=>{
+    res.send(usuario[].idade)
 })
 
 module.exports = app
